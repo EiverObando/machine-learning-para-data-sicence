607e7ad9bfa48a7636a418098dfea4cd76d0fcc3
eiver obando <eoobandoramirez103293@umanizales.edu.co>
2025-02-27 00:59:45 -0500

    verificación de configuraciones

---
+++ preprocesado.py
@@ -0,0 +1,9 @@
+# plantilla de preprocesado de datos
+# Autor: Eiver Obando
+
+# Importar librerias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print("Librerias importadas")
